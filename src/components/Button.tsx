0ed84d93bd89b4c2da25861fab8e9d0ed2e544cc
Hajrajadoon <48354586+Hajrajadoon@users.noreply.github.com>
2025-08-24 23:48:37 +0500

    Update Button.tsx

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,4 +1,10 @@
 import React from 'react';
-export const Button = ({ children, className, onClick }: any) => (
-  <button onClick={onClick} className={`px-4 py-2 bg-green-500 text-white rounded ${className}`}>{children}</button>
+
+export const Button = ({ children, onClick }: any) => (
+  <button
+    onClick={onClick}
+    className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
+  >
+    {children}
+  </button>
 );
