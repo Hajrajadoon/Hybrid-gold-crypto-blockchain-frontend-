9a61e3e7166aa6bc97d2b92be2d7e36f0e0bb0c4
Hajrajadoon <48354586+Hajrajadoon@users.noreply.github.com>
2025-08-24 23:52:19 +0500

    Update WalletConnect.tsx

--- src/components/WalletConnect.tsx
+++ src/components/WalletConnect.tsx
@@ -1,4 +1,9 @@
 import React from 'react';
-export default function WalletConnect() {
-  return <Button>Connect Wallet</Button>;
-}
+
+const WalletConnect = () => (
+  <button className="px-4 py-2 bg-green-600 text-white rounded hover:bg-green-700">
+    Connect Wallet
+  </button>
+);
+
+export default WalletConnect;
