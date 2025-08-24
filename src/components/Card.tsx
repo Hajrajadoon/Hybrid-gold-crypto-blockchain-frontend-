f6e95c16a61e0e4cd2ee1d092eba8d75ccf7b7c1
Hajrajadoon <48354586+Hajrajadoon@users.noreply.github.com>
2025-08-24 23:16:28 +0500

    Update Card.tsx

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
-export const Card = ({ children, className }: any) => <div className={`p-4 bg-white shadow rounded ${className}`}>{children}</div>;
+
+export const Card = ({ children, className }: any) => (
+  <div className={`p-4 bg-white shadow rounded ${className}`}>{children}</div>
+);
+
 export const CardHeader = ({ children }: any) => <div className="mb-2">{children}</div>;
 export const CardTitle = ({ children }: any) => <h2 className="font-bold">{children}</h2>;
 export const CardContent = ({ children }: any) => <div>{children}</div>;
